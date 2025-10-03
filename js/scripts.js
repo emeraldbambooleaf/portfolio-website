d9b166c59e8ddbc6a674147f0d7e91f5139a331c
emeraldbambooleaf <emerald.bamboo.learning@gmail.com>
2025-10-03 15:59:53 -0700

    toggle header

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,17 @@
 
-console.log("Hello visitors! Welcome to my website.");
+/*
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    x.className += ' responsive';
+  }
+
+  */
+
+  function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
